7a0181ac78406fb4bf8c4f4413a4372cb745f23b
João Vitor <joaovitorfelixcontato@gmail.com>
2025-04-25 18:28:58 -0300

    feat: add delete student route

--- server/src/factories/student.ts
+++ server/src/factories/student.ts
@@ -1,15 +1,26 @@
 import { PostgresAdapter } from "@/adapters/PostgresAdapter";
+import { DeleteStudentController } from "@/controllers/student";
 import { CreateStudentController } from "@/controllers/student/CreateStudentController";
 import { pool } from "@/db/db";
 import { StudentRepository } from "@/repositories/StudentRepository";
 import { CreateStudentUseCase } from "@/useCases/student/CreateStudentUseCase";
+import { DeleteStudentUseCase } from "@/useCases/student/DeleteStudentUseCase";
+
+const db = new PostgresAdapter(pool);
+const studentRepository = new StudentRepository(db);
 
 export function makeCreateStudentController() {
-  const db = new PostgresAdapter(pool);
-  const studentRepository = new StudentRepository(db);
   const createStudentUseCase = new CreateStudentUseCase(studentRepository);
   const createStudentController = new CreateStudentController(
     createStudentUseCase
   );
   return createStudentController;
+}
+
+export function makeDeleteStudentController() {
+  const deleteStudentUseCase = new DeleteStudentUseCase(studentRepository);
+  const deleteStudentController = new DeleteStudentController(
+    deleteStudentUseCase
+  );
+  return deleteStudentController;
 }
--- server/src/routes/student.ts
+++ server/src/routes/student.ts
@@ -1,4 +1,7 @@
-import { makeCreateStudentController } from "@/factories/student";
+import {
+  makeCreateStudentController,
+  makeDeleteStudentController
+} from "@/factories/student";
 import { Router } from "express";
 
 export const studentsRouter = Router();
@@ -6,5 +9,11 @@ export const studentsRouter = Router();
 studentsRouter.post("/", async (req, res) => {
   const createStudentController = makeCreateStudentController();
   const { statusCode, body } = await createStudentController.execute(req);
+  res.status(statusCode).send(body);
+});
+
+studentsRouter.delete("/:ra", async (req, res) => {
+  const deleteStudentController = makeDeleteStudentController();
+  const { statusCode, body } = await deleteStudentController.execute(req);
   res.status(statusCode).send(body);
 });
