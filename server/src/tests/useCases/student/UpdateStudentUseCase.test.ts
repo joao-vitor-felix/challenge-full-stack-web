0e0396e90268887fa25bd4ca2a7647d391a3d6ad
João Vitor <joaovitorfelixcontato@gmail.com>
2025-04-26 13:51:37 -0300

    test:(UpdateStudentUseCase): should return updated user successfully

---
+++ server/src/tests/useCases/student/UpdateStudentUseCase.test.ts
@@ -0,0 +1,44 @@
+import { UpdateStudentSchema } from "@/schemas/student";
+import { StudentRepositoryStub } from "@/tests/stubs/StudentRepositoryStub";
+import { Student } from "@/types/Student";
+import { UpdateStudentUseCase } from "@/useCases/student/UpdateStudentUseCase";
+import { faker } from "@faker-js/faker";
+
+describe("UpdateStudentUseCase", () => {
+  function makeSut() {
+    const studentRepository = new StudentRepositoryStub();
+    const sut = new UpdateStudentUseCase(studentRepository);
+    return {
+      studentRepository,
+      sut
+    };
+  }
+
+  const ra = faker.string.numeric(11);
+  const params: UpdateStudentSchema = {
+    name: faker.person.fullName(),
+    email: faker.internet.email()
+  };
+
+  it("should return updated user successfully", async () => {
+    const { sut } = makeSut();
+
+    const student = await sut.execute(ra, params);
+
+    expect(student).toEqual<Student>({
+      ra,
+      cpf: expect.any(String),
+      name: params.name ?? expect.any(String),
+      email: params.email ?? expect.any(String)
+    });
+  });
+
+  it.todo("should call update with correct params", async () => {});
+
+  it.todo(
+    "should throw StudentNotFoundError if no student is returned",
+    async () => {}
+  );
+
+  it.todo("should throw if repository throws", async () => {});
+});
