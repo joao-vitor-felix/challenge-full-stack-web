7faae9877e46b329ddfb86d4cae183ac432a1cfc
João Vitor <joaovitorfelixcontato@gmail.com>
2025-04-27 18:31:45 -0300

    test(SignInController): add body validation tests

--- server/src/tests/controllers/auth/SignInController.test.ts
+++ server/src/tests/controllers/auth/SignInController.test.ts
@@ -54,7 +54,71 @@ describe("SignInController", () => {
     );
   });
 
-  it.todo.each([])("should return 400 when $scenario", async () => {});
+  it.each([
+    {
+      scenario: "email is not a string",
+      httpRequest: {
+        body: {
+          ...httpRequest.body,
+          email: 1
+        }
+      },
+      errorMessage: /email must be a string/i
+    },
+    {
+      scenario: "email is invalid",
+      httpRequest: {
+        body: {
+          ...httpRequest.body,
+          email: "invalid-email"
+        }
+      },
+      errorMessage: /invalid email/i
+    },
+    {
+      scenario: "password is not a string",
+      httpRequest: {
+        body: {
+          ...httpRequest.body,
+          password: 1
+        }
+      },
+      errorMessage: /password must be a string/i
+    },
+    {
+      scenario: "password is less than 8 characters",
+      httpRequest: {
+        body: {
+          ...httpRequest.body,
+          password: "1234567"
+        }
+      },
+      errorMessage: /at least 8 characters/i
+    },
+    {
+      scenario: "a not allowed field is provided",
+      httpRequest: {
+        body: {
+          ...httpRequest.body,
+          extra: 1
+        }
+      },
+      errorMessage: /not allowed/i
+    }
+  ])(
+    "should return 400 when $scenario",
+    async ({ httpRequest, errorMessage }) => {
+      const { sut } = makeSut();
+
+      const response = (await sut.execute(
+        httpRequest as HttpRequest
+      )) as ErrorResponse;
+
+      expect(response.statusCode).toBe(400);
+      expect(response.body.message).toMatch(errorMessage);
+      expect(response.body.code).toBe("INVALID_REQUEST");
+    }
+  );
 
   it("should return 404 when use case throws StaffNotFoundError", async () => {
     const { sut, signInUseCase } = makeSut();
