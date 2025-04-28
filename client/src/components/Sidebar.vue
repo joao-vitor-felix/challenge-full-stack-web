e793998921ab63e770c3f5e432abe0ae8a799734
João Vitor <joaovitorfelixcontato@gmail.com>
2025-04-28 17:07:56 -0300

    feat(client): add Sidebar to Home

---
+++ client/src/components/Sidebar.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import Logo from "@/components/Logo.vue";
+import { authStore } from "@/store/authStore";
+import { useRoute, useRouter } from "vue-router";
+
+const route = useRoute();
+const router = useRouter();
+const isActive = route.path === "/";
+
+function handleLogout() {
+  authStore.logout();
+  router.push({ name: "sign-in" });
+}
+</script>
+
+<template>
+  <v-navigation-drawer mobile-breakpoint="sm">
+    <Logo />
+    <div class="d-flex align-center">
+      <span class="w-100 text-center font-weight-bold text-primary">Módulo acadêmico</span>
+    </div>
+    <v-list>
+      <v-list-item prepend-icon="mdi-account" :active="isActive" active-color="primaryLight"
+        >Alunos</v-list-item
+      >
+    </v-list>
+
+    <template v-slot:append>
+      <div class="pa-2">
+        <v-btn block color="accentLight" @click="handleLogout">Sair</v-btn>
+      </div>
+    </template>
+  </v-navigation-drawer>
+</template>
