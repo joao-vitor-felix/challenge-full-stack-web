27900c65a11e651589f4adc64b90108450998272
João Vitor <joaovitorfelixcontato@gmail.com>
2025-04-28 17:00:37 -0300

    feat(client): add navigation guards

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,19 +1,42 @@
-import SignInVue from "@/pages/SignIn/SignIn.vue";
-import SignUpVue from "@/pages/SignUp/SignUp.vue";
+import Home from "@/pages/Home/Home.vue";
+import SignIn from "@/pages/SignIn/SignIn.vue";
+import SignUp from "@/pages/SignUp/SignUp.vue";
+import { authStore } from "@/store/authStore";
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: "/",
+      component: Home,
+      name: "home"
+    },
+    {
       path: "/sign-up",
-      component: SignUpVue
+      component: SignUp,
+      name: "sign-up"
     },
     {
       path: "/sign-in",
-      component: SignInVue
+      component: SignIn,
+      name: "sign-in"
     }
   ]
+});
+
+router.beforeEach(to => {
+  if (!authStore.isAuthenticated && to.name !== "sign-in" && to.name !== "sign-up") {
+    return {
+      name: "sign-in"
+    };
+  }
+
+  if (authStore.isAuthenticated && (to.name === "sign-in" || to.name === "sign-up")) {
+    return {
+      name: "home"
+    };
+  }
 });
 
 export default router;
